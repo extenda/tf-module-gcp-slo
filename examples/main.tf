9cc770e5ced721a9651ad13020c52faaede5e2c6
Anton-L-Ext <anton.lorge@extendaretail.com>
2022-08-18 11:17:07 +0200

    chore: Add telemetry in example

--- examples/main.tf
+++ examples/main.tf
@@ -2,5 +2,6 @@ module "slo" {
   source                = "../"
   monitoring_project_id = "monitoring-project-id"
   service_name          = "my-awesome-service"
+  telemetry_resource_name = "//container.googleapis.com/projects/project-id/locations/location/clusters/k8s-cluster/k8s/namespaces/example"
   slos                  = yamldecode(file("slos.yaml"))
 }
