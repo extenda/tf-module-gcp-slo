c1c460ec1a0cf6220bfa679882f942a6cd1f90a1
Anton-L-Ext <anton.lorge@extendaretail.com>
2022-08-18 10:25:22 +0200

    chore: Remove unnecessary quotes

--- vars.tf
+++ vars.tf
@@ -1,27 +1,27 @@
-variable "monitoring_project_id" {
+variable monitoring_project_id {
   type        = string
   description = "Project ID to create monitoring resources in"
   default     = "hiiretail-monitoring-prod-6500"
 }
 
-variable "service_name" {
+variable service_name {
   type        = string
   description = "Display name of the custom service"
 }
 
-variable "telemetry_resource_name" {
+variable telemetry_resource_name {
   type        = string
   description = "The full name of the resource that defines this service"
   default     = ""
 }
 
-variable "notification_channels" {
+variable notification_channels {
   type        = list(any)
   description = "List of notificaton channel IDs"
   default     = []
 }
 
-variable "slos" {
+variable slos {
   description = "Configuration for SLO"
   type        = any
 }
