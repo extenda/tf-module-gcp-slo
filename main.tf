8b4ccf4ff4a1e83a27507724a5db26198273f891
Anton-L-Ext <anton.lorge@extendaretail.com>
2023-01-23 16:10:11 +0100

    feat: Add nc by name

--- main.tf
+++ main.tf
@@ -1,7 +1,16 @@
-resource "google_monitoring_custom_service" "custom_service" {
-  project      = var.monitoring_project_id
+locals {
+  fallback_notification_channels = [for nc in var.fallback_notification_channels : try(var.notification_channel_ids[nc], nc)]
+  default_alert_conditions = [
+    { name : "2% of error budget consumed in 1 hour", threshold_value : 14, time : "3600s" },
+    { name : "5% of error budget consumed in 6 hours", threshold_value : 6, time : "21600s" },
+    /* { name : "10% of error budget consumed in 3 days", threshold_value : 1, time : "259200s" }, */
+  ]
+}
+
+resource "google_monitoring_custom_service" "service" {
+  project      = var.project
   display_name = var.service_name
-  user_labels  = var.user_labels
+  user_labels  = var.default_user_labels
 
   telemetry {
     resource_name = var.telemetry_resource_name
@@ -11,80 +20,79 @@ resource "google_monitoring_custom_service" "custom_service" {
 resource "google_monitoring_slo" "slo" {
   for_each = { for slo in var.slos : slo.slo_id => slo }
 
-  service             = google_monitoring_custom_service.custom_service.service_id
-  project             = var.monitoring_project_id
+  service             = google_monitoring_custom_service.service.service_id
+  project             = var.project
   slo_id              = each.value.slo_id
   display_name        = each.value.display_name
   goal                = each.value.goal
-  user_labels         = var.user_labels
-  calendar_period     = lookup(each.value, "calendar_period", null)
-  rolling_period_days = lookup(each.value, "rolling_period_days", null)
+  user_labels         = merge(var.default_user_labels, try(each.value.user_labels, {}))
+  calendar_period     = try(each.value.calendar_period, null)
+  rolling_period_days = try(each.value.rolling_period_days, null)
+
 
   dynamic "basic_sli" {
-    for_each = lookup(each.value, "type") == "basic_sli" ? ["yes"] : []
+    for_each = each.value.type == "basic_sli" ? [1] : []
     content {
       latency {
-        threshold = lookup(each.value, "latency_threshold", null)
+        threshold = try(each.value.latency_threshold, null)
       }
     }
   }
 
   dynamic "request_based_sli" {
-    for_each = lookup(each.value, "type") == "request_based_sli" ? ["yes"] : []
+    for_each = each.value.type == "request_based_sli" ? [1] : []
     content {
-
       dynamic "distribution_cut" {
-        for_each = lookup(each.value, "method", null) == "distribution_cut" ? ["yes"] : []
+        for_each = each.value.method == "distribution_cut" ? [1] : []
         content {
-          distribution_filter = lookup(each.value, "metric_filter", null)
+          distribution_filter = try(each.value.metric_filter, null)
           range {
-            min = lookup(each.value, "range_min", null)
-            max = lookup(each.value, "range_max", null)
+            min = try(each.value.range_min, null)
+            max = try(each.value.range_max, null)
           }
         }
       }
 
       dynamic "good_total_ratio" {
-        for_each = lookup(each.value, "method", null) == "good_total_ratio" ? ["yes"] : []
+        for_each = each.value.method == "good_total_ratio" ? [1] : []
         content {
-          good_service_filter  = lookup(each.value, "good_service_filter", null)
-          bad_service_filter   = lookup(each.value, "bad_service_filter", null)
-          total_service_filter = lookup(each.value, "total_service_filter", null)
+          good_service_filter  = try(each.value.good_service_filter, null)
+          bad_service_filter   = try(each.value.bad_service_filter, null)
+          total_service_filter = try(each.value.total_service_filter, null)
         }
       }
     }
   }
 
   dynamic "windows_based_sli" {
-    for_each = lookup(each.value, "type") == "windows_based_sli" ? ["yes"] : []
+    for_each = each.value.type == "windows_based_sli" ? [1] : []
     content {
-      window_period = lookup(each.value, "window_period", null)
-
-      good_bad_metric_filter = lookup(each.value, "method", null) == "boolean_filter" ? lookup(each.value, "metric_filter", null) : null
+      window_period          = try(each.value.window_period, null)
+      good_bad_metric_filter = each.value.method == "boolean_filter" ? try(each.value.metric_filter, null) : null
 
       dynamic "good_total_ratio_threshold" {
-        for_each = lookup(each.value, "method", null) == "performance_window" ? ["yes"] : []
+        for_each = try(each.value.method, null) == "performance_window" ? [1] : []
         content {
-          threshold = lookup(each.value, "threshold", null)
-
+          threshold = try(each.value.threshold, null)
           dynamic "performance" {
-            for_each = lookup(each.value, "method_performance", null) == "good_total_ratio" || lookup(each.value, "method_performance", null) == "distribution_cut" ? ["yes"] : []
+            for_each = try(each.value.method_performance, null) == "good_total_ratio" || try(each.value.method_performance, null) == "distribution_cut" ? [1] : []
             content {
               dynamic "good_total_ratio" {
-                for_each = lookup(each.value, "method_performance", null) == "good_total_ratio" ? ["yes"] : []
+                for_each = try(each.value.method_performance, null) == "good_total_ratio" ? [1] : []
                 content {
-                  good_service_filter  = lookup(each.value, "good_service_filter", null)
-                  bad_service_filter   = lookup(each.value, "bad_service_filter", null)
-                  total_service_filter = lookup(each.value, "total_service_filter", null)
+                  good_service_filter  = try(each.value.good_service_filter, null)
+                  bad_service_filter   = try(each.value.bad_service_filter, null)
+                  total_service_filter = try(each.value.total_service_filter, null)
                 }
               }
+
               dynamic "distribution_cut" {
-                for_each = lookup(each.value, "method_performance", null) == "distribution_cut" ? ["yes"] : []
+                for_each = try(each.value.method_performance, null) == "distribution_cut" ? [1] : []
                 content {
-                  distribution_filter = lookup(each.value, "metric_filter", null)
+                  distribution_filter = try(each.value.metric_filter, null)
                   range {
-                    min = lookup(each.value, "range_min", null)
-                    max = lookup(each.value, "range_max", null)
+                    min = try(each.value.range_min, null)
+                    max = try(each.value.range_max, null)
                   }
                 }
               }
@@ -92,13 +100,13 @@ resource "google_monitoring_slo" "slo" {
           }
 
           dynamic "basic_sli_performance" {
-            for_each = lookup(each.value, "method_performance", null) == "basic_sli_performance" ? ["yes"] : []
+            for_each = try(each.value.method_performance, null) == "basic_sli_performance" ? [1] : []
             content {
-              method   = lookup(each.value, "api_method", null)
-              location = lookup(each.value, "api_location", null)
-              version  = lookup(each.value, "api_version", null)
+              method   = try(each.value.api_method, null)
+              location = try(each.value.api_location, null)
+              version  = try(each.value.api_version, null)
               latency {
-                threshold = lookup(each.value, "latency_threshold", null)
+                threshold = try(each.value.latency_threshold, null)
               }
             }
           }
@@ -106,23 +114,23 @@ resource "google_monitoring_slo" "slo" {
       }
 
       dynamic "metric_mean_in_range" {
-        for_each = lookup(each.value, "method", null) == "metric_mean_in_range" ? ["yes"] : []
+        for_each = try(each.value.method, null) == "metric_mean_in_range" ? [1] : []
         content {
-          time_series = lookup(each.value, "metric_filter", null)
+          time_series = try(each.value.metric_filter, null)
           range {
-            min = lookup(each.value, "range_min", null)
-            max = lookup(each.value, "range_max", null)
+            min = try(each.value.range_min, null)
+            max = try(each.value.range_max, null)
           }
         }
       }
 
       dynamic "metric_sum_in_range" {
-        for_each = lookup(each.value, "method", null) == "metric_sum_in_range" ? ["yes"] : []
+        for_each = try(each.value.method, null) == "metric_sum_in_range" ? [1] : []
         content {
-          time_series = lookup(each.value, "metric_filter", null)
+          time_series = try(each.value.metric_filter, null)
           range {
-            min = lookup(each.value, "range_min", null)
-            max = lookup(each.value, "range_max", null)
+            min = try(each.value.range_min, null)
+            max = try(each.value.range_max, null)
           }
         }
       }
@@ -130,34 +138,34 @@ resource "google_monitoring_slo" "slo" {
   }
 }
 
-resource "google_monitoring_alert_policy" "alert_policy" {
-  for_each              = { for slo in var.slos : slo.slo_id => slo }
-  project               = var.monitoring_project_id
-  notification_channels = var.notification_channels
-  user_labels           = var.user_labels
-  display_name          = lookup(lookup(each.value, "alert", {}), "name", "[P2] ${var.service_name} SLO | ${each.value.slo_id} - High burnrate ")
-  combiner              = lookup(lookup(each.value, "alert", {}), "combiner", "OR")
+resource "google_monitoring_alert_policy" "alert" {
+  for_each = { for slo in var.slos : slo.slo_id => slo if try(slo.alert, true) }
+
+  project      = var.project
+  display_name = "[P2] ${var.service_name} SLO | ${each.value.slo_id} - High burnrate "
+  combiner     = "OR"
+  user_labels  = null
+  notification_channels = try(
+    [for nc in each.value.notification_channels : try(var.notification_channel_ids[nc], nc)],
+    local.fallback_notification_channels,
+  )
 
   dynamic "conditions" {
-    for_each = { for alert in lookup(each.value, "conditions", [
-      { name : "2% of error budget consumed in 1 hour", threshold_value : 14, time : "3600s" },
-      { name : "5% of error budget consumed in 6 hours", threshold_value : 6, time : "21600s" },
-      { name : "10% of error budget consumed in 3 days", threshold_value : 1, time : "259200s" },
-    ]) : alert.name => alert }
+    for_each = local.default_alert_conditions
     content {
       display_name = conditions.value.name
       condition_threshold {
-        comparison      = lookup(conditions.value, "comparison", "COMPARISON_GT")
-        duration        = lookup(conditions.value, "duration", "0s")
-        threshold_value = lookup(conditions.value, "threshold_value")
-        filter          = "select_slo_burn_rate(${lookup(lookup(google_monitoring_slo.slo, each.value.slo_id), "name")}, ${conditions.value.time})"
+        comparison      = "COMPARISON_GT"
+        duration        = "60s"
+        threshold_value = conditions.value.threshold_value
+        filter          = "select_slo_burn_rate(${google_monitoring_slo.slo[each.value.slo_id].name}, ${conditions.value.time})"
       }
     }
   }
 
   documentation {
     mime_type = "text/markdown"
-    content   = var.documentation
+    content   = try(each.value.documentation, var.default_alert_documentation)
   }
 
   depends_on = [google_monitoring_slo.slo]
--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,20 @@
-output custom_service {
+output "custom_service" {
   description = "The custom service"
-  value       = google_monitoring_custom_service.custom_service.id
+  value       = google_monitoring_custom_service.service.id
 }
 
-output slos {
+output "slos" {
   description = "The SLOs"
   value = {
     for k, v in google_monitoring_slo.slo :
+    k => v.id
+  }
+}
+
+output "alert" {
+  description = "The burn-rate alert"
+  value = {
+    for k, v in google_monitoring_alert_policy.alert :
     k => v.id
   }
 }
